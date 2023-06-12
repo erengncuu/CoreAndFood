708d5d0ebd58edfc0982c40400c7e7afb6e2f0fa
sezgin <erengncuu@outlook.com>
2023-06-12 16:52:05 +0300

    repositories

---
+++ CoreAndFood/Repositories/FoodRepository.cs
@@ -0,0 +1,34 @@
+﻿using CoreAndFood.Models;
+
+namespace CoreAndFood.Repositories
+{
+	public class FoodRepository
+	{
+		Context c = new Context();
+		
+		public void FoodAdd(Food f)
+		{
+			c.Foods.Add(f);
+			c.SaveChanges();
+		}
+		public void FoodRemove(Food f)
+		{
+			c.Foods.Remove(f);
+			c.SaveChanges();
+		}
+		public void FoodUpdate(Food f)
+		{
+			c.Foods.Update(f);
+			c.SaveChanges();
+		}
+		public List<Food> FoodList()
+		{
+			return c.Foods.ToList();
+		}
+		public void GetFood(int id)
+		{
+			c.Foods.Find(id);
+			c.SaveChanges();
+		}
+	}
+}
