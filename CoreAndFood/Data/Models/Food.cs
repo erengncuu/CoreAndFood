5c8311e4cbf8c3d39cbe662c5fbef5f7434a7512
sezgin <erengncuu@outlook.com>
2023-06-12 10:13:10 +0300

    modelsler eklendi veriler içine işlendi context sınıfı konuldu ilişkiler kuruldu

---
+++ CoreAndFood/Data/Models/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreAndFood.Data.Models
+{
+	public class Food
+	{
+		public int FoodId { get; set; }
+		public string Name { get; set; }
+		public string ShortDescription { get; set; }
+		public string LongDescription { get; set; }
+		public double Price { get; set; }
+		public string ImageURL { get; set; }
+		public string ThumbNailImageURL { get; set; }
+		public int Stock { get; set; }
+
+        public int CategoryId { get; set; }
+		public virtual Category Category { get; set; }//ilişkiyi kurabilmek için
+    }
+}
