185caee219bcad95ef9fca9e875875e389b29a44
sezgin <erengncuu@outlook.com>
2023-06-14 14:52:05 +0300

    Add category e uzunluk kısıtlaması getirildi

--- CoreAndFood/Models/Category.cs
+++ CoreAndFood/Models/Category.cs
@@ -6,6 +6,7 @@ namespace CoreAndFood.Models
     {
         public int CategoryId { get; set; }
         [Required(ErrorMessage ="Category Name not be empty")]
+        [StringLength(20,ErrorMessage ="Please only enter 4-20 characters",MinimumLength =4)]
         public string? CategoryName { get; set; }
         public string? CategoryDescription { get; set; }
 
