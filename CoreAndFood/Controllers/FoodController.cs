4a98f9b3e26fb5f327feb638dc71a9af80e05d37
sezgin <erengncuu@outlook.com>
2023-06-14 12:10:09 +0300

    Food Add sayfası yapıldı

--- CoreAndFood/Controllers/FoodController.cs
+++ CoreAndFood/Controllers/FoodController.cs
@@ -1,14 +1,26 @@
-﻿using CoreAndFood.Repositories;
+﻿using CoreAndFood.Models;
+using CoreAndFood.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreAndFood.Controllers
 {
 	public class FoodController : Controller
 	{
+		FoodRepository foodRepository = new FoodRepository();
 		public IActionResult Index()
-		{	
-			FoodRepository foodRepository = new FoodRepository();
+		{		
 			return View(foodRepository.TList("Category"));
 		}
-	}
+		[HttpGet]
+        public IActionResult FoodAdd()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult FoodAdd(Food f)
+		{
+			foodRepository.TAdd(f);
+			return RedirectToAction("Index");
+		}
+    }
 }
